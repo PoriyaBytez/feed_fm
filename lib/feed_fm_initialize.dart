43d7958041a61c0a3651dee3ca5bfcecf1f9d371
urvi2108 <urvi@byteztech.in>
2025-09-23 16:14:43 +0530

    Add FeedFm class for audio feed management with initialization and control methods

---
+++ lib/feed_fm_initialize.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/services.dart';
+
+class FeedFm {
+  static const _channel = MethodChannel('feedfm');
+
+  static Future<bool> initialize({
+    required String token,
+    required String secret,
+  }) async {
+    final success = await _channel.invokeMethod('initialize', {
+      'token': token,
+      'secret': secret,
+    });
+    return success;
+  }
+
+  static Future<void> play() => _channel.invokeMethod('play');
+  static Future<void> pause() => _channel.invokeMethod('pause');
+  static Future<void> skip() => _channel.invokeMethod('skip');
+
+  static Future<List<String>> stations() async {
+    final result = await _channel.invokeMethod('stations');
+    return List<String>.from(result ?? []);
+  }
+}
